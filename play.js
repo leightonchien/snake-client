89894779b4b6e437fd4ef46915ab9ad7e7de6f29
leightonchien <leightonchien@gmail.com>
2021-03-11 06:57:23 +0000

    refactor

--- play.js
+++ play.js
@@ -1,21 +1,3 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '10.0.0.69',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on("data", data => {
-    console.log(data);
-  });
-  return conn;
-}
-
 const { connect } = require("./client");
 console.log('Connecting ...');
 connect();
