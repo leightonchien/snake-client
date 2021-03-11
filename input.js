3eb861720bfd7cbcd8a29d036bc9166c3fb5e451
leightonchien <leightonchien@gmail.com>
2021-03-11 15:59:39 +0000

    WASD movement

--- input.js
+++ input.js
@@ -1,6 +1,11 @@
-function setupInput() {
+// Stores the active TCP connection object.
+let connection;
+
+function setupInput(conn) {
+    connection = conn;
     const stdin = process.stdin;
     stdin.on("data", handleUserInput);
+    stdin.on("data", movementInput);
     stdin.setRawMode(true);
     stdin.setEncoding("utf8");
     stdin.resume();
@@ -9,9 +14,27 @@ function setupInput() {
   
   function handleUserInput(key) {
     if (key === "\u0003") {
+        
       process.exit();
     }
   }
-  setupInput();
   
+
+  function movementInput(key) {
+    if (key === "a") {
+      connection.write("Move: left");
+    }
+    if (key === "d") {
+      connection.write("Move: right");
+    }
+    if (key === "w") {
+      connection.write("Move: up");
+    }
+    if (key === "s") {
+      connection.write("Move: down");
+    }
+  }
+  
+  // setupInput();
+  module.exports = { connection };
   module.exports = { setupInput };
