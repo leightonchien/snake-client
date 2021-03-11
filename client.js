c5108fefdf890836c616b57c25ca6edb7f6b0829
leightonchien <leightonchien@gmail.com>
2021-03-11 15:13:11 +0000

    client.js refactor then export IP & PORT to constants.js

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 // Establishes connection with the game server
   
 const connect = function() {
   const conn = net.createConnection({
-    host: "10.0.0.69",
-    port: 50541
+    host: IP,
+    port: PORT
   });
 
   conn.on("connect", () => {
