b5d29597444a65520b3a7165323baa40a2de49f5
leightonchien <leightonchien@gmail.com>
2021-03-11 15:45:37 +0000

    export setUpInput

--- play.js
+++ play.js
@@ -1,24 +1,27 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
+
 console.log('Connecting ...');
 connect();
 
+setupInput();
 
 /**
  * Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
-function setupInput() {
-  const stdin = process.stdin;
-  stdin.on("data", handleUserInput);
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-}
+// function setupInput() {
+//   const stdin = process.stdin;
+//   stdin.on("data", handleUserInput);
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+//   return stdin;
+// }
 
-function handleUserInput(key) {
-  if (key === "\u0003") {
-    process.exit();
-  }
-}
-setupInput();
+// function handleUserInput(key) {
+//   if (key === "\u0003") {
+//     process.exit();
+//   }
+// }
+// setupInput();
